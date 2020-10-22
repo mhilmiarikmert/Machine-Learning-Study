818c7678700bcf551ac7bfef179e682220bb8653
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-10-22 16:50:28 +0300

    data reading with pandas library

--- Data-Preprocessing/first.py
+++ Data-Preprocessing/first.py
@@ -1,3 +1,21 @@
+#!usr/bin/env python3
+#coding: utf-8
+"""
+Created on Thu Oct 2020
+
+@author: mharikmert
+"""
+#libraries
 import pandas as pd
 import numpy as np
-import matlotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('data.csv')
+print(data)
+
+height = data['boy'] # can be used one bracket for one column
+height = data[['boy']]
+print(height)
+
+height_weight = data[['boy', 'kilo']] # can not be used one bracket for two column
+print(height_weight)
