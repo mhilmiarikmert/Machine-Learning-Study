87973aeb850a40d88dae48628c60ac33b436365d
mhilmiarikmert <mhilmiarikmert@gmail.com>
2020-10-23 11:47:04 +0300

    object syntax in python

--- Data-Preprocessing/first.py
+++ Data-Preprocessing/first.py
@@ -11,11 +11,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('data.csv')
-print(data)
+#print(data)
 
 height = data['boy'] # can be used one bracket for one column
 height = data[['boy']]
-print(height)
+#print(height)
 
 height_weight = data[['boy', 'kilo']] # can not be used one bracket for two column
-print(height_weight)
+#print(height_weight)
+class person:
+  def __init__(self, height, weight):
+    self.height = height
+    self.weight = weight
+
+  def printProp(self):
+    print('height {} , weight {}'.format(self.height ,self.weight))
+
+person = person(1.80, 80)
+person.printProp()
+
+
+
+
